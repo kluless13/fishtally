39c7607fe545db3545a540a1a7634b2a519ddc78
kluless13 <100767970+kluless13@users.noreply.github.com>
2023-12-15 12:11:35 +0530

    readme updates

--- README.md
+++ README.md
@@ -7,6 +7,118 @@ Objective: Use yolov8 to build a tracking model. Model weights can be used by th
     <img src="https://github.com/kluless13/paper2/blob/main/Assets/tang-tracker%20(2).gif" width="49%"/>
 </div>
 
+Certainly! Below is a setup guide suitable for inclusion in a `README.md` file for your fish tallying tool, `fishtally.py`. This guide covers the necessary steps to get the tool up and running.
+
+---
+
+## Setup Guide
+
+### Prerequisites
+
+Ensure you have the following prerequisites installed on your system:
+
+- Python 3.6 or higher
+- Git
+- Pip (Python package manager)
+
+### Installation Steps
+
+1. **Clone the Repository**
+
+   First, clone the repository containing the `fishtally.py` tool and its associated files to your local machine.
+
+   ```bash
+   git clone https://github.com/kluless13/paper2.git
+   cd paper2
+   ```
+
+2. **Install ByteTrack**
+
+   The `fishtally.py` tool relies on ByteTrack for object tracking. Run the provided setup script to clone and set up ByteTrack.
+
+   ```bash
+   chmod +x setup.sh
+   ```
+
+3. **Install Python Dependencies**
+
+   Install the required Python packages using the `requirements.txt` file.
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+   This command installs all necessary Python packages for `fishtally.py`.
+
+4. **Verification**
+
+   To verify that the installation is successful, you can run a simple test command or check the versions of critical components like Python, Git, and Pip.
+
+   ```bash
+   python --version
+   git --version
+   pip --version
+   ```
+
+Certainly! Here's a guide for running `fishtally.py`, which can be included in your `README.md` file or other documentation. This guide will provide instructions on how to use the command-line interface (CLI) of the tool.
+
+---
+
+## Running `fishtally.py`
+
+### Usage Instructions
+
+The `fishtally.py` tool is a command-line application that can be used for fish counting in videos using different detection methods (single line, multiple lines, and polygon). 
+
+To use the tool, follow these steps:
+
+1. **Open the Terminal**
+
+   Open a terminal window and navigate to the directory where `fishtally.py` is located.
+
+2. **Running the Tool**
+
+   Use the following command structure to run the tool:
+
+   ```bash
+   python fishtally.py --model_weights <path_to_weights> --source_video <path_to_source_video> --target_video <path_to_output_video> --detector_type <detector_type> [additional_arguments]
+   ```
+
+   Replace `<path_to_weights>`, `<path_to_source_video>`, and `<path_to_output_video>` with the respective paths. For `<detector_type>`, choose from `single_line`, `multi_line`, or `polygon`.
+
+   - For `single_line` and `multi_line`, you need to specify line coordinates. 
+   - For `polygon`, provide the polygon points.
+
+3. **Example Commands**
+
+   Here are some example commands for each detector type:
+
+   - **Single Line:**
+     ```bash
+     python fishtally.py --model_weights weights.pt --source_video source.mp4 --target_video output.mp4 --detector_type single_line --line_start 100 200 --line_end 300 400
+     ```
+
+   - **Multiple Lines:**
+     ```bash
+     python fishtally.py --model_weights weights.pt --source_video source.mp4 --target_video output.mp4 --detector_type multi_line --line_start 100 200 --line_end 300 400 --line_start2 500 600 --line_end2 700 800
+     ```
+
+   - **Polygon:**
+     ```bash
+     python fishtally.py --model_weights weights.pt --source_video source.mp4 --target_video output.mp4 --detector_type polygon --polygon_points 100 200 300 400 500 600 700 800
+     ```
+
+    Replace the coordinates in these examples with those relevant to your specific use case.
+
+4. **Viewing the Results**
+
+   After running the command, the tool will process the video and output the results to the path specified in `--target_video`. Check this file to view the results of the fish counting process.
+
+### Notes
+
+- Ensure that the paths to the model weights and videos are correct.
+- The coordinates for lines or polygons should be adjusted based on the requirements of your specific task.
+
 Model trained on a multiclass dataset (listed below), 
 
 #### Dataset info:
--- fishtally.py
+++ fishtally.py
@@ -1,51 +1,35 @@
-from ultralytics import YOLO
-import cv2
-import numpy as np
 import argparse
-import os
-import sys
-from supervision.draw.color import ColorPalette
-from supervision.geometry.dataclasses import Point
-from supervision.video.dataclasses import VideoInfo
-from supervision.video.source import get_video_frames_generator
-from supervision.video.sink import VideoSink
-from supervision.notebook.utils import show_frame_in_notebook
-from supervision.tools.detections import Detections, BoxAnnotator
-from supervision.tools.line_counter import LineCounter, LineCounterAnnotator
-
-# Function for single line threshold counting
-def single_line_threshold(video_path, weights_path, line_start, line_end):
-    # Code from notebook 1
-
-# Function for multiple line threshold counting
-def multi_line_threshold(video_path, weights_path, lines):
-    # Code from notebook 2
-
-# Function for polygon threshold counting
-def polygon_threshold(video_path, weights_path, polygon_points):
-    # Code from notebook 3
+from single_line import single_line_threshold
+from multi_line import multi_line_threshold
+from polygon import polygon_threshold
 
 def main():
     parser = argparse.ArgumentParser(description='Fish Tallying Tool')
-    parser.add_argument('--video_path', required=True, help='Path to the video file')
-    parser.add_argument('--weights_path', required=True, help='Path to the YOLO weights')
-    parser.add_argument('--detector_type', choices=['single_line', 'multi_line', 'polygon'], required=True, help='Type of detector')
-    parser.add_argument('--line_points', nargs='+', help='Points for lines or polygon')
+    parser.add_argument('--model_weights', required=True, help='Path to the model weights file')
+    parser.add_argument('--source_video', required=True, help='Path to the source video file')
+    parser.add_argument('--target_video', required=True, help='Path for the output video file')
+    parser.add_argument('--detector_type', required=True, choices=['single_line', 'multi_line', 'polygon'], help='Type of detector to use')
+    
+    # For simplicity, threshold settings are added as optional arguments. You can further customize as needed.
+    parser.add_argument('--line_start', nargs=2, type=int, help='Start coordinates of the line (x, y)', default=[0, 0])
+    parser.add_argument('--line_end', nargs=2, type=int, help='End coordinates of the line (x, y)', default=[100, 100])
+    parser.add_argument('--polygon_points', nargs='+', type=int, help='List of points (x, y) defining the polygon', default=[0, 0, 100, 0, 100, 100, 0, 100])
+    
     args = parser.parse_args()
 
     if args.detector_type == 'single_line':
-        # Assuming line_points for single line is in the format: x1 y1 x2 y2
-        line_start = Point(args.line_points[0], args.line_points[1])
-        line_end = Point(args.line_points[2], args.line_points[3])
-        single_line_threshold(args.video_path, args.weights_path, line_start, line_end)
+        # Call single_line_threshold function
+        single_line_threshold(args.model_weights, args.source_video, args.target_video, tuple(args.line_start), tuple(args.line_end))
     elif args.detector_type == 'multi_line':
-        # Convert line_points to a list of start and end points for each line
-        lines = [(Point(args.line_points[i], args.line_points[i+1]), Point(args.line_points[i+2], args.line_points[i+3])) for i in range(0, len(args.line_points), 4)]
-        multi_line_threshold(args.video_path, args.weights_path, lines)
+        # Call multi_line_threshold function
+        # Note: You need to adjust this part to pass the correct format of line points for your multi_line_threshold function
+        multi_line_threshold(args.model_weights, args.source_video, args.target_video, [tuple(args.line_start), tuple(args.line_end)])
     elif args.detector_type == 'polygon':
-        # Convert line_points to a list of points defining the polygon
-        polygon_points = [Point(args.line_points[i], args.line_points[i+1]) for i in range(0, len(args.line_points), 2)]
-        polygon_threshold(args.video_path, args.weights_path, polygon_points)
+        # Call polygon_threshold function
+        # Convert flat list of coordinates to list of tuples
+        it = iter(args.polygon_points)
+        polygon_points = list(zip(it, it))
+        polygon_threshold(args.model_weights, args.source_video, args.target_video, polygon_points)
 
 if __name__ == '__main__':
     main()
